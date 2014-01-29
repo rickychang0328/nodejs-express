4820603f2f4928b8bf27b138eda1009d7f17b5e1
rickychang0328 <rickychang0328@gmail.com>
2014-01-29 14:28:24 +0800

    -a -v # -v

--- app.js
+++ app.js
@@ -9,10 +9,27 @@ var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 var hello = require('./routes/hello');
-
+var version = require('./routes/version');
 var app = express();
-
+var api = require('./routes/api');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/vcard');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+  console.log('MongoDB: connected.');	
+});
 // all environments
+var Schema = mongoose.Schema;
+var vcardSchema = mongoose.Schema({
+    name: String,
+    nickname: String,
+    tel: String
+})
+
+app.db = {
+	model: mongoose.model('Vcard', vcardSchema)
+};
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -32,6 +49,13 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/hello', hello.index);
+app.get('/version', version.index);
+// app.get('/info/:username', user.info);
+// REST API
+app.post('/1/user', api.create);
+app.get('/1/user', api.read);
+app.put('/1/user/:nickname', api.update);
+app.delete('/1/user/:nickname', api.delete);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,12 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+exports.info = function(req, res){
+  //res.send("respond with a resource:"+req.params.username);
+  res.render('info',{username:req.params.username});
+  // console.log(req.params.username);
+  // var obj = { 
+  // 	username:req.params.username
+  // };
+  // req.send(obj);
+};
